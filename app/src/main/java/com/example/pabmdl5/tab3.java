c700dc65142b436160688ff016600e0a323f45ce
izzyraenaldy <35583421+izzyraenaldy27@users.noreply.github.com>
2024-07-03 14:14:44 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/pabmdl5/tab3.java
@@ -0,0 +1,13 @@
+package com.example.pabmdl5;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tab3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab3);
+    }
+}
